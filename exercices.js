fb16bbfcf519796092a7cefc9e47d8f5f61ddf1e
Njogu-Ndegwa <dennisndegwa001@gmail.com>
2020-08-18 11:32:36 +0300

    Sequece of multiplications and additions that results to a number

--- exercices.js
+++ exercices.js
@@ -82,3 +82,20 @@ function power(base, exponent) {
     }
 }
 
+// A function that when given a number tries to fing a sequence of additions and mulplications that lead to that number
+
+function findSolution(target) {
+    function find(current, history) {
+        if(current == target) {
+            return history;
+        } else if (current > target) {
+            return null
+        } else {
+            return find(current + 5, `(${history} + 5)`) ||
+                   find(current * 3, `(${history} * 3)`);
+        }
+    }
+
+    return find(1, '1');
+}
+
